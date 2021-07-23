400a674aa5bdf8cb5c1815f0c80a68662bd98d2f
Filipe Encarnacao <filipe11402@outlook.pt>
2021-07-23 20:15:59 +0100

    added bootstrap cdn and create brand view

--- Dealership.App/Dealership.App/Controllers/BrandController.cs
+++ Dealership.App/Dealership.App/Controllers/BrandController.cs
@@ -12,5 +12,10 @@ namespace Dealership.App.Controllers
         {
             return View();
         }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
     }
 }
