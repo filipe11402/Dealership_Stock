633dd60ea90fc27f8b8c8051049779e139a66b7a
Filipe Encarnacao <filipe11402@outlook.pt>
2021-07-29 06:02:14 +0100

    added viewmodels

---
+++ Dealership.App/Dealership.App/Models/Car/DeleteCarViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dealership.App.Models.Car
+{
+    public class DeleteCarViewModel
+    {
+        public int CarId { get; set; }
+    }
+}
