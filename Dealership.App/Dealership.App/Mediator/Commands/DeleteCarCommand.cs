82f5986b30991d485374995eef7045e120f2f721
Filipe Encarnacao <filipe11402@outlook.pt>
2021-07-30 21:37:24 +0100

    added delete functionality no testing

---
+++ Dealership.App/Dealership.App/Mediator/Commands/DeleteCarCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dealership.App.Mediator.Commands
+{
+    public class DeleteCarCommand : IRequest<bool>
+    {
+        public int CarId { get; }
+
+        public DeleteCarCommand(int carToDeleteId)
+        {
+            this.CarId = carToDeleteId;
+        }
+    }
+}
---
+++ Dealership.App/Dealership.App/Mediator/Handlers/DeleteCarCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Dealership.App.Mediator.Commands;
+using Dealership.Domain.Entities;
+using Dealership.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Dealership.App.Mediator.Handlers
+{
+    public class DeleteCarCommandHandler : IRequestHandler<DeleteCarCommand, bool>
+    {
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteCarCommandHandler(IMapper mapper, IUnitOfWork unitOfWork)
+        {
+            this._mapper = mapper;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public Task<bool> Handle(DeleteCarCommand request, CancellationToken cancellationToken)
+        {
+            Car carToDelete = this._unitOfWork.Cars.GetById(request.CarId);
+        }
+    }
+}
